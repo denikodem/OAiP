88a8f633612fa310475fa8133df9e5686f46372f
denikodem <denikodem@yandex.ru>
2026-01-24 13:50:35 +0900

    основа

--- 2СПИ_Онодал_Л9/Button/Button.py
+++ 2СПИ_Онодал_Л9/Button/Button.py
@@ -1,18 +1,25 @@
 class WebButton:
     def __init__(self):
-
         # атрибуты кнопки
-        self.toggler = False
-        self.color = None
+        self.toggle = False
         self.scale = [1.0, 1.0]
         self.text = None
+        self.color = None
 
-    def click_button(self):
-        if not self.toggler:
-            self.toggler = True
-            print("Кнопка включена")
-        else:
-            self.toggler = False
-            print("Кнопка выключена")
+    def toggle_button(self) -> bool:
+        self.toggle = not self.toggle
+        return self.toggle
 
-    
+    def scale_button(self) -> list:
+        x, y = map(int, input("Укажите размер кнопки X и Y через пробел\n>>>>> ").split(' '))
+        self.scale = [x, y]
+        return self.scale
+
+    def text_button(self) -> str:
+        self.text = str(input())
+        return self.text
+
+    def color_button(self):
+        r, g, b = map(int, input("Укажите три цвета RGB через пробел\n>>>>>").split(" "))
+        self.color = [r, g, b]
+        return self.color
