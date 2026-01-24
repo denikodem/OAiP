c6d6e0702c9eb81aeb309d041324ce0dee2bdf9b
denikodem <denikodem@yandex.ru>
2026-01-24 14:14:27 +0900

    подготовка main и абстракция Button

--- 2СПИ_Онодал_Л9/Button/Button.py
+++ 2СПИ_Онодал_Л9/Button/Button.py
@@ -1,25 +1,38 @@
-class WebButton:
+from abc import ABC, abstractmethod
+
+class ButtonAction(ABC):
+    def __init__(self, config: "ButtonConfig"):
+        self.config = config
+    @abstractmethod
+    def execute(self):
+        pass
+
+class ButtonConfig:
     def __init__(self):
         # атрибуты кнопки
         self.toggle = False
-        self.scale = [1.0, 1.0]
-        self.text = None
-        self.color = None
+        self.scale = (1.0, 1.0)
+        self.text = ""
+        self.color = (0, 0, 0)
 
-    def toggle_button(self) -> bool:
-        self.toggle = not self.toggle
-        return self.toggle
+class TogglerButton(ButtonAction):
+    def execute(self) -> bool:
+        self.config.toggle = not self.config.toggle
+        return self.config.toggle
 
-    def scale_button(self) -> list:
+class ScaleButton(ButtonAction):
+    def execute(self) -> tuple[int, int]:
         x, y = map(int, input("Укажите размер кнопки X и Y через пробел\n>>>>> ").split(' '))
-        self.scale = [x, y]
-        return self.scale
+        self.config.scale = (x, y)
+        return self.config.scale
 
-    def text_button(self) -> str:
-        self.text = str(input())
-        return self.text
+class TextButton(ButtonAction):
+    def execute(self) -> str:
+        self.config.text = str(input())
+        return self.config.text
 
-    def color_button(self):
+class ColorButton(ButtonAction):
+    def execute(self) -> tuple[int, int, int]:
         r, g, b = map(int, input("Укажите три цвета RGB через пробел\n>>>>>").split(" "))
-        self.color = [r, g, b]
-        return self.color
+        self.config.color = (r, g, b)
+        return self.config.color
--- 2СПИ_Онодал_Л9/Button/main.py
+++ 2СПИ_Онодал_Л9/Button/main.py
@@ -1,4 +1,10 @@
-from Button import WebButton
-obj = WebButton()
+from Button import (
+    ButtonConfig,
+    TogglerButton,
+    ScaleButton,
+    TextButton,
+    ColorButton,
+)
+#obj = ()
 if __name__ == "__main__":
     pass
