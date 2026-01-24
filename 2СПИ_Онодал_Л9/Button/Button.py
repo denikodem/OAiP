fbabb39247df36456dc276a221c36b9f16620be9
denikodem <denikodem@yandex.ru>
2026-01-24 14:35:24 +0900

    complete

--- 2СПИ_Онодал_Л9/Button/Button.py
+++ 2СПИ_Онодал_Л9/Button/Button.py
@@ -1,24 +1,36 @@
 from abc import ABC, abstractmethod
 
+
 class ButtonAction(ABC):
     def __init__(self, config: "ButtonConfig"):
         self.config = config
+
     @abstractmethod
     def execute(self):
         pass
+
 
 class ButtonConfig:
     def __init__(self):
         # атрибуты кнопки
         self.toggle = False
         self.scale = (1.0, 1.0)
-        self.text = ""
-        self.color = (0, 0, 0)
+        self.text = "Кнопка"
+        self.color = (255, 255, 255)
+
+    def give_config(self):
+        return f'''
+        "Состояние кнопки": {self.toggle},
+        "Размер кнопки": {self.scale},
+        "Тест кнопки": {self.text},
+        "Цвет кнопки": {self.color},
+        '''
 
 class TogglerButton(ButtonAction):
     def execute(self) -> bool:
         self.config.toggle = not self.config.toggle
         return self.config.toggle
+
 
 class ScaleButton(ButtonAction):
     def execute(self) -> tuple[int, int]:
@@ -26,10 +38,12 @@ class ScaleButton(ButtonAction):
         self.config.scale = (x, y)
         return self.config.scale
 
+
 class TextButton(ButtonAction):
     def execute(self) -> str:
         self.config.text = str(input())
         return self.config.text
+
 
 class ColorButton(ButtonAction):
     def execute(self) -> tuple[int, int, int]:
--- 2СПИ_Онодал_Л9/Button/main.py
+++ 2СПИ_Онодал_Л9/Button/main.py
@@ -1,3 +1,5 @@
+from fastapi.params import Cookie
+
 from Button import (
     ButtonConfig,
     TogglerButton,
@@ -5,6 +7,44 @@ from Button import (
     TextButton,
     ColorButton,
 )
-#obj = ()
+
+
+class ButtonManager:
+    def __init__(self):
+        self.config = ButtonConfig()
+        self.buttons = {
+            "1": TogglerButton(self.config),
+            "2": ScaleButton(self.config),
+            "3": TextButton(self.config),
+            "4": ColorButton(self.config)
+        }
+
+    def run(self):
+        running = True
+        while running:
+            choice = input("""
+            Выберите действие с кнопкой:
+            
+            1. Включить/Выключить
+            2. Изменить размеры кнопки
+            3. Поменять текст кнопки
+            4. Поменять цвет кнопки
+            5. Показать конфиг кнопки
+            6. Выход
+            
+>>>>""")
+            if choice == "6":
+                running = not running
+            elif choice == "5":
+                print(ButtonConfig.give_config(self.config))
+            elif choice in self.buttons:
+                result = self.buttons[choice].execute()
+                print("Получилось: ", result)
+            else:
+                print("Повторите попытку")
+                continue
+
+
 if __name__ == "__main__":
-    pass
+    ButtonManager().run()
+    input('Нажмите Enter для выхода...')
