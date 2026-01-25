b96b66aa2062ef89a5ee2b0315a7b83eb386595f
denikodem <denikodem@yandex.ru>
2026-01-25 23:00:27 +0900

    пред последнее глобальное изменение

--- 2СПИ_Онодал_Л9/Chest/Chest.py
+++ 2СПИ_Онодал_Л9/Chest/Chest.py
@@ -12,12 +12,12 @@ class ChestAction(ABC):
 
 
 class ChestConfig:
-    def __init__(self, name):
-        self.material = ("Дерево", "Медь", "Железо", "Золото", "Алмазы")
+    MATERIALS = ("Дерево", "Медь", "Железо", "Золото", "Алмазы")
 
+    def __init__(self, name):
         self.name = name
         self.is_open = True
-        self.material_chest = choice(self.material)
+        self.material_chest = choice(self.MATERIALS)
         self.double_chest = False
 
         self.items_inside = []
@@ -35,6 +35,7 @@ class ChestConfig:
 class SwitchChest(ChestAction):
     def execute(self):
         self.config.is_open = not self.config.is_open
+        return "Сундук был открыт" if self.config.is_open else "Сундук был закрыт"
 
     def __str__(self):
         return "Сундук открыт" if self.config.is_open else "Сундук закрыт"
@@ -42,18 +43,41 @@ class SwitchChest(ChestAction):
 
 class SetMaterial(ChestAction):
     def execute(self):
-        self.config.material_chest = self.config.material[int(input("""\nВведите материал сундука
+        try:
+            choice = int(input("""\nВведите материал сундука
 1. Дерево
 2. Медь
 3. Железо
 4. Золото
-5. Алмазы""")) - 1]
+5. Алмазы"""))
+            self.config.material_chest = self.config.MATERIALS[choice - 1]
+        except (ValueError, IndexError):
+            return "Неверный код"
+        return f'Материал сундука изменен на {self.config.material_chest}'
 
     def __str__(self):
         return f"Материал сундука: {self.config.material_chest}"
 
-class SetScale(ChestAction):
+
+class SwitchDoubleChest(ChestAction):
     def execute(self):
         self.config.double_chest = not self.config.double_chest
         return "Сундук теперь двойной" if self.config.double_chest else "Сундук теперь одинарный"
 
+    def __str__(self):
+        return "Сундук двойной" if self.config.double_chest else "Сундук одинарный"
+
+
+class AddInChest(ChestAction):
+    def execute(self):
+        if self.config.is_open:
+            items = input('Напишите через пробел названия предметов>>>>').split()
+            if not items:
+                return "Предметы не были введены"
+            self.config.items_inside.extend(items)
+            return f"Предметы: {'; '.join(items)} добавлены в сундук"
+        else:
+            return f'Сундук был закрыт и предметы не были добавлены.'
+
+    def __str__(self):
+        return f"В сундуке: {', '.join(self.config.items_inside) if self.config.items_inside else 'пусто'}"
--- 2СПИ_Онодал_Л9/Chest/main.py
+++ 2СПИ_Онодал_Л9/Chest/main.py
@@ -2,8 +2,19 @@ from Chest import (
     ChestConfig,
     SwitchChest,
     SetMaterial,
+    SwitchDoubleChest,
+
 )
-choices = """
+
+class ManagerClass:
+
+    name_chest = str(input("Дайте название своему сундуку\n>>> "))
+
+    def __init__(self):
+        self.obj = ChestConfig(self.name_chest)
+        self.action = SetMaterial(self.obj)
+
+        self._choices = """
     Выберете действие:
 1. Открыть сундук
 2. Поменять материал сундука
@@ -11,24 +22,27 @@ choices = """
 4. Посмотреть что внутри
 5. Добавить предмет
 6. Удалить предмет
+7. Выход
 """
-map_action = {
-    "1": SwitchChest,
-    "2": SetMaterial,
-    "3": '',
-}
+    map_action = {
+        "1": SwitchChest,
+        "2": SetMaterial,
+        "3": SwitchDoubleChest,
+        "4":
+    }
 
-def main():
-    print("\n\nДОБРО ПОЖАЛОВАТЬ В CHEST FROM GAME\n")
-    name_chest = str(input("Дайте название своему сундуку\n>>> "))
-
-    obj = ChestConfig(name_chest)
-    action = SetMaterial(obj)
-    running = True
-
-    while running:
-        choice = int(input(choices))
+    def run(self):
+        print("\n\nДОБРО ПОЖАЛОВАТЬ В CHEST FROM GAME\n")
+        while True:
+            choice = input(self._choices)
+            if choice in self.map_action:
+                result = self.map_action[choice].execute(self.action)
+                print(result)
+            elif choice == "7":
+                break
+            else:
+                continue
 
 if __name__ == "__main__":
-    main()
+    ManagerClass.run()
     input("\n\nНажмите Enter чтобы выйти...")
